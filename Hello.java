070288e1d2d8ad65cb0af7fe4d05b9fc10e394c7
june757 <69877393+june757@users.noreply.github.com>
2020-08-18 19:19:22 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/********************************************************
+ * Java Program Name 
+ *********************************************************
+ * Student Name Ronald Bowens
+ * Date 8/18/2020
+ * Course Number and Section CMSC 255-C90
+ *********************************************************/
+
+public class Hello { 
+ 	public static void main(String[] args) {
+	
+		System.out.println("Ronald Bowens");
+		System.out.println("Lab 01");
+		System.out.println("Indroduction to the Course");
+		System.out.println("8/18/2020");
+		System.out.println("255-C90");
+		System.out.println();
+		}
+}
